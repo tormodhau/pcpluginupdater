381516a8d0b5612943c29172c6228ffb5033cdb6
Tormod Haugene <tormod@resight.no>
2017-08-29 14:22:08 +0200

    Flyway now initiates runtime

--- src/main/java/no/resight/powercatch/pcpluginupdater/startupTasks/RunDatabaseMigration.java
+++ src/main/java/no/resight/powercatch/pcpluginupdater/startupTasks/RunDatabaseMigration.java
@@ -2,7 +2,6 @@ package no.resight.powercatch.pcpluginupdater.startupTasks;
 
 import org.apache.log4j.Logger;
 import org.flywaydb.core.Flyway;
-import org.postgresql.ds.PGSimpleDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
@@ -13,29 +12,39 @@ import javax.sql.DataSource;
 public class RunDatabaseMigration {
 
     private static final Logger log = Logger.getLogger(RunDatabaseMigration.class);
+    private final Environment environment;
 
 
     @Autowired
-    public RunDatabaseMigration(Flyway flyway) {
+    public RunDatabaseMigration(Environment environment) {
+        this.environment = environment;
+
+        Flyway flyway = new Flyway();
+
+        String encoding = flyway.getEncoding();
+
         log.info("************** RUNNING DATABASE MIGRATIONS ****************");
+        log.info("************** " + encoding + " ****************");
+
 
 //        Flyway flyway = new Flyway();
-        flyway.setBaselineVersionAsString("123Version");
-        flyway.setBaselineOnMigrate(true);
-        flyway.setLocations("path/To/Migrations/Folder");
-        flyway.setDataSource(getDataSource());
-        flyway.migrate();
+//        flyway.setBaselineVersionAsString("123Version");
+//        flyway.setBaselineOnMigrate(true);
+//        flyway.setLocations("path/To/Migrations/Folder");
+//        flyway.setDataSource(getDataSource());
+//        flyway.migrate();
     }
 
     private DataSource getDataSource() {
-        // https://jdbc.postgresql.org/documentation/81/ds-ds.html
-        PGSimpleDataSource pgSimpleDataSource = new PGSimpleDataSource();
-        pgSimpleDataSource.setServerName("SERVER NAME");
-        pgSimpleDataSource.setDatabaseName("DATABASE NAME");
-        pgSimpleDataSource.setPortNumber(80808080);
-        pgSimpleDataSource.setUser("USERNAME");
-        pgSimpleDataSource.setPassword("PASSWORD");
-        return pgSimpleDataSource;
+//        // https://jdbc.postgresql.org/documentation/81/ds-ds.html
+//        PGSimpleDataSource pgSimpleDataSource = new PGSimpleDataSource();
+//        pgSimpleDataSource.setServerName("SERVER NAME");
+//        pgSimpleDataSource.setDatabaseName("DATABASE NAME");
+//        pgSimpleDataSource.setPortNumber(80808080);
+//        pgSimpleDataSource.setUser("USERNAME");
+//        pgSimpleDataSource.setPassword("PASSWORD");
+//        return pgSimpleDataSource;
+        return null;
     }
 
 }
